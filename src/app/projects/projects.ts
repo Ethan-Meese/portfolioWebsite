017becc64f14732489379a48be1b20824305d355
Ethan Meese <meeseethan@gmail.com>
2025-11-19 22:09:33 -0500

    Added some styling to Projects tab and edited other tabs a little

--- src/app/projects/projects.ts
+++ src/app/projects/projects.ts
@@ -1,42 +1,43 @@
 export const PROJECTS = [
-    {
-        id: 1,
-        name: "Dungeon Crawler Web Game",
-        stacks: ["C#", ".Net", "Angular"],
-        description: "Description...",
-        img: "assets/portrait.jpg",
-        githubLink: "https://github.com/Ethan-Meese/Dungeon-Game-Web"
-    },
-    {
-        id: 2,
-        name: "E-Commerce Website",
-        stacks: ["Java", "MVN", "MySQL", "Springboot"],
-        description: "Description",
-        img: "assets/portrait.jpg",
-        githubLink: ""
-    },
-    {
-        id: 3,
-        name: "Dungeon Crawler CLI",
-        stacks: ["C#"],
-        description: "Description",
-        img: "assets/portrait.jpg",
-        githubLink: "https://github.com/Ethan-Meese/dungeon-game"
-    },
-    {
-        id: 4,
-        name: "Unity 2D Side Scroller",
-        stacks: ["C#", "Unity"],
-        description: "Description",
-        img: "assets/portrait.jpg",
-        githubLink: ""
-    },
-    {
-        id: 5,
-        name: "BlackJack CLI",
-        stacks: ["Python"],
-        description: "Description",
-        img: "assets/portrait.jpg",
-        githubLink: ""
-    }
-]
+  {
+    id: 1,
+    name: 'Dungeon Crawler Web Game',
+    stacks: ['C#', '.Net', 'Angular'],
+    description:
+      'This project consists of a .NET backend using C# for the logic along with Angular as the frontend. It is a fully playable web-game that has random enemies, rooms, and multiple attacks. On top of that it uses a dynamic frontend to display the information and make the gameplay more immersive.sdaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+    img: 'assets/portrait.jpg',
+    githubLink: 'https://github.com/Ethan-Meese/Dungeon-Game-Web',
+  },
+  {
+    id: 2,
+    name: 'E-Commerce Website',
+    stacks: ['Java', 'MVN', 'MySQL', 'Springboot'],
+    description: 'Description',
+    img: 'assets/portrait.jpg',
+    githubLink: '',
+  },
+  {
+    id: 3,
+    name: 'Dungeon Crawler CLI',
+    stacks: ['C#'],
+    description: 'Description',
+    img: 'assets/portrait.jpg',
+    githubLink: 'https://github.com/Ethan-Meese/dungeon-game',
+  },
+  {
+    id: 4,
+    name: 'Unity 2D Side Scroller',
+    stacks: ['C#', 'Unity'],
+    description: 'Description',
+    img: 'assets/portrait.jpg',
+    githubLink: '',
+  },
+  {
+    id: 5,
+    name: 'BlackJack CLI',
+    stacks: ['Python'],
+    description: 'Description',
+    img: 'assets/portrait.jpg',
+    githubLink: '',
+  },
+];
